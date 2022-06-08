d58e653511e2a18e0b6095f449b376ca4210858b
Justin Pham <justin.tk.pham@gmail.com>
2022-06-08 18:19:26 +1000

    add draw grid from input number

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function generateGrid(number) {
+const input = document.querySelector('#input');
+input.addEventListener('change', event => generateGrid(event.target.value));
 
-    let grid = document.querySelector('grid');
+function generateGrid(number) {
+    console.log('entered generateGrid');
+    clearGrid();
+    let grid = document.querySelector('#grid');
 
     for (let i = 0; i < number; i++) {
         let row = document.createElement('div');
@@ -9,9 +13,22 @@ function generateGrid(number) {
         for (let i = 0; i < number; i++) {
             let cell = document.createElement('div');
             cell.classList.add('cell');
+            cell.style.width = `${Math.floor(1048/number)}px`;
+            cell.style.height = `${Math.floor(1048/number)}px`;
             row.appendChild(cell);
         }
 
         grid.appendChild(row);
+    }
+}
+
+function clearGrid() {
+    console.log('entered clearGrid');
+    let grid = document.querySelector('#grid');
+    let child = grid.lastElementChild;
+
+    while (child) {
+        grid.removeChild(child);
+        child = grid.lastElementChild;
     }
 }
